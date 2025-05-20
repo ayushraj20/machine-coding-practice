73a784dbce4fd706e840092c3f9bf0e4b5cf47f7
ayushraj20 <rajrakshal20@gmail.com>
2025-05-20 17:38:41 +0530

    dvb

--- src/components/TypeAhead.jsx
+++ src/components/TypeAhead.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/TypeAhead.css';
 
 const TypeAhead = () => {
+  const [results, setResults] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [cache, setCache] = useState({});
+
+  useEffect(() => {
+    let timeout = setTimeout(getRecipeData, 500);
+
+    return () => clearTimeout(timeout);
+  }, [searchText]);
+
+  const getRecipeData = async () => {
+    if (cache[searchText]) {
+      console.log('returned from cache', searchText);
+      setResults(cache[searchText]);
+      return;
+    }
+
+    console.log('api called', searchText);
+    const data = await fetch(
+      `https://dummyjson.com/recipes/search?q=${searchText}`
+    );
+    const res = await data.json();
+
+    setResults(res?.recipes);
+    setCache((prev) => ({ ...prev, [searchText]: res?.recipes }));
+  };
+
   return (
-    <div>
-      TypeAhead
-      <span>oindf</span>
+    <div className="main-container">
+      <h3>TypeAhead (AutoComplete)</h3>
+      <div>
+        <input
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          className="input-box"
+        />
+
+        <div className="results-container">
+          {results?.map((result) => (
+            <div className="result-item" key={result.id}>
+              {result?.name}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
