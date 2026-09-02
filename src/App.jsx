1fe102ce0c6818673373051cd6f87e4987b787b2
K-Nomad <agraved27@gmail.com>
2026-09-02 18:29:41 +0530

    sdfdf

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
       {/* <Timer /> */}
       {/* <RandomGridCohesity /> */}
       {/* <TypeAhead /> */}
-      <TrafficLights />
+      {/* <TrafficLights /> */}
     </>
   );
 }
