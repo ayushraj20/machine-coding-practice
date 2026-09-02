0e0b70c8510e45f424005de6e79925934fb195de
K-Nomad <agraved27@gmail.com>
2026-09-02 18:20:12 +0530

    new osdnfoidnf

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import '../styles/ProgressBar.css';
 
-const ProgressBar = ({ progress = 43 }) => {
+const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
+  const intervalRef = useRef();
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(intervalRef.current);
+          return 100;
+        }
+        return prev + 10;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
   return (
     <div className="bar-container">
       <div className="bar" style={{ width: `${progress}%` }}>
